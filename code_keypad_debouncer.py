0e42b58e6a0b8bce6d3094b6917550f4025c3f6e
Neradoc <neraOnGit@ri1.fr>
2021-09-26 20:21:23 +0200

    debouncer library for the built-in keypad module

---
+++ code_keypad_debouncer.py
@@ -0,0 +1,27 @@
+import board
+import keypad
+import time
+
+from keypad_debouncer import Debouncer
+
+KEY_PINS = (board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5, board.KEY6, board.KEY7, board.KEY8, board.KEY9, board.KEY10, board.KEY11, board.KEY12)
+keypad_keys = keypad.Keys(KEY_PINS, value_when_pressed=False, pull=True)
+
+db = Debouncer(keypad_keys, hold_time=4.0)
+
+while True:
+    db.update()
+    if db.rose(0):
+        print("Pressed Key 0")
+    if db.fell(0):
+        print(f"Held Key 0 for {db.last_duration(0) / 1000}s")
+    if db.held(0):
+        print("Holding long Key 0")
+
+    roses = db.rose()
+    if len(roses):
+        print("roses", repr(roses))
+
+    held = db.held()
+    if len(held):
+        print("helds", repr(held))
---
+++ keypad_debouncer.py
@@ -0,0 +1,109 @@
+"""
+Docstring debouncer module
+"""
+import supervisor
+from micropython import const
+
+STATE = const(0)
+ROSE = const(1)
+FELL = const(2)
+# TIME-2 = const(3)
+# TIME-1 = const(4)
+TIME = const(5)
+HELD = const(6)
+HOLD_TIME = const(7)
+
+class Debouncer:
+    """
+    Docstring Debouncer class
+    """
+    def __init__(self, keys, hold_time=1.0, double_delay=0):
+        self.keypad = keys
+        self.hold_time = int(hold_time * 1000)
+        self.double_delay = double_delay
+        self.timestamp = 0
+        self.values = [
+            [ False, False, False, 0, 0, 0, False, hold_time]
+            for _ in range(keys.key_count)
+        ]
+
+    def update(self):
+        for pos in range(len(self.values)):
+            self.values[pos][ROSE] = False
+            self.values[pos][FELL] = False
+        events = []
+        while event := self.keypad.events.get():
+            events.append(event)
+        self.timestamp = supervisor.ticks_ms()
+        for event in events:
+            pos = event.key_number
+            self.values[pos][STATE] = event.pressed
+            self.values[pos][ROSE] = event.pressed
+            self.values[pos][FELL] = event.released
+            self.values[pos][TIME-2] = self.values[pos][TIME-1]
+            self.values[pos][TIME-1] = self.values[pos][TIME]
+            self.values[pos][TIME] = event.timestamp
+            self.values[pos][HELD] = False
+
+    def current_duration(self, pos):
+        return self.timestamp - self.values[pos][TIME]
+
+    def last_duration(self, pos):
+        return self.timestamp - self.values[pos][TIME-1]
+
+    def rose(self, pos=None):
+        if pos is None:
+            return [
+                pp for pp, val in enumerate(self.values)
+                if val[ROSE]
+            ]
+        else:
+            return self.values[pos][ROSE]
+
+    def fell(self, pos=None):
+        if pos is None:
+            return [
+                pp for pp, val in enumerate(self.values)
+                if val[FELL]
+            ]
+        else:
+            return self.values[pos][FELL]
+
+    def pressed(self, pos=None):
+        if pos is None:
+            return [
+                pp for pp, val in enumerate(self.values)
+                if val[STATE]
+            ]
+        else:
+            return self.values[pos][STATE]
+
+    def hold_time(self, pos, value):
+        self.values[pos][HOLD_TIME] = value
+
+    def set_hold_times(self, value):
+        for pos in range(len(self.values)):
+            self.values[pos][HOLD_TIME] = value
+
+    def held(self, pos=None):
+        if pos is None:
+            return [
+                pp for pp, val in enumerate(self.values)
+                if self.held(pp)
+            ]
+        if self.values[pos][HELD]:
+            return False
+        duration = int(self.values[pos][HOLD_TIME] * 1000)
+        dt = self.timestamp - self.values[pos][TIME]
+        if self.values[pos][STATE] and dt >= duration:
+            self.values[pos][HELD] = True
+            return True
+        return False
+
+    def __repr__(self):
+        out = ["{"]
+        for pp, val in enumerate(self.values):
+            out.append(f"    {pp}: {val}")
+        out.append("}")
+        return "\n".join(out)
+
